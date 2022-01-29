408c4cb2fe1331d39d527460ac27c3969eaa9206
Jeremy Tdr <jeremy.tdr@gmail.com>
2022-01-29 16:23:16 +0100

    changes to img route

--- .eleventy.js
+++ .eleventy.js
@@ -2,8 +2,8 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget("./src/scss/");
 
   eleventyConfig.addPassthroughCopy("./src/sass/style.css");
-  eleventyConfig.addPassthroughCopy("./src/assets/");
-  eleventyConfig.addPassthroughCopy("./src/js");
+  eleventyConfig.addPassthroughCopy("./src/assets");
+  eleventyConfig.addPassthroughCopy("./src/js/");
   return {
     dir: {
       input: "src",
