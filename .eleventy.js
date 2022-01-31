487a99801a8324d2939dee4e414b5a32e37708d0
Jeremy Tdr <jeremy.tdr@gmail.com>
2022-01-31 17:30:49 +0100

    Admin folder added to eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/assets");
   eleventyConfig.addPassthroughCopy("./src/js");
   eleventyConfig.addPassthroughCopy("./src/galery");
+  eleventyConfig.addPassthroughCopy("./src/admin");
   return {
     dir: {
       input: "src",
