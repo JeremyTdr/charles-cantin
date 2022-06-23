28bb0e84a61761cbce3835a9a41c41ed35d64a1c
Jeremy Tdr <jeremy.tdr@gmail.com>
2022-06-23 17:12:21 +0200

    changes to eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,11 @@
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addWatchTarget("./src/scss/");
+  eleventyConfig.addWatchTarget("/src/scss/");
 
-  eleventyConfig.addPassthroughCopy("./src/sass/style.css");
-  eleventyConfig.addPassthroughCopy("./src/assets");
-  eleventyConfig.addPassthroughCopy("./src/js");
-  eleventyConfig.addPassthroughCopy("./src/galery");
-  eleventyConfig.addPassthroughCopy("./src/admin");
+  eleventyConfig.addPassthroughCopy("/src/sass/style.css");
+  eleventyConfig.addPassthroughCopy("/src/assets");
+  eleventyConfig.addPassthroughCopy("/src/js");
+  eleventyConfig.addPassthroughCopy("/src/galery");
+  eleventyConfig.addPassthroughCopy("/src/admin");
   return {
     dir: {
       input: "src",
