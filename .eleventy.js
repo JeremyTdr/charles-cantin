d7cc4594aa03805923aaf7e99204c94c56b76d01
Jeremy Tdr <jeremy.tdr@gmail.com>
2022-06-23 17:28:20 +0200

    Add some changes in eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,12 @@
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addWatchTarget("/src/scss/");
+  eleventyConfig.addWatchTarget("./src/scss/");
 
-  eleventyConfig.addPassthroughCopy("/src/sass/style.css");
-  eleventyConfig.addPassthroughCopy("/src/assets");
-  eleventyConfig.addPassthroughCopy("/src/assets/img");
-  eleventyConfig.addPassthroughCopy("/src/js");
-  eleventyConfig.addPassthroughCopy("/src/galery");
-  eleventyConfig.addPassthroughCopy("/src/admin");
+  eleventyConfig.addPassthroughCopy("./src/sass/style.css");
+  eleventyConfig.addPassthroughCopy("./src/assets");
+  eleventyConfig.addPassthroughCopy("./src/assets/img");
+  eleventyConfig.addPassthroughCopy("./src/js");
+  eleventyConfig.addPassthroughCopy("./src/galery");
+  eleventyConfig.addPassthroughCopy("./src/admin");
   return {
     dir: {
       input: "src",
